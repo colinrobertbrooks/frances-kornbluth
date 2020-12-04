f8308581df84e0886189c5679a672ca4ce54eb15
Colin Brooks <colinrobertbrooks@gmail.com>
2020-12-04 09:47:42 -0500

    fill out not found page

--- src/components/pages/NotFound.tsx
+++ src/components/pages/NotFound.tsx
@@ -1,4 +1,18 @@
 import React from 'react';
 import { Page } from '../layout';
+import { Link, Url } from '../router';
+import { Row, Col, Heading, Paragraph } from '../styled';
 
-export const NotFound: React.FC = () => <Page title="Not Found" />;
+export const NotFound: React.FC = () => (
+  <Page title="Not Found">
+    <Row>
+      <Col md={12}>
+        <Heading className="mb-3">Not Found</Heading>
+        <Paragraph>That page doesn&#39;t exist.</Paragraph>
+        <Paragraph>
+          <Link to={Url.HomePage}>Back to home</Link>
+        </Paragraph>
+      </Col>
+    </Row>
+  </Page>
+);
