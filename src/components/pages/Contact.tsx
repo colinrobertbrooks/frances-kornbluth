9691d366d36280e47adf5561697d9ad5820b7db4
Colin Brooks <colinrobertbrooks@gmail.com>
2020-12-04 09:20:27 -0500

    refactor contact page

--- src/components/pages/Contact.tsx
+++ src/components/pages/Contact.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import styled from 'styled-components';
 import { Page } from '../layout';
 import { Link, Url } from '../router';
 import {
@@ -14,28 +15,22 @@ export const Contact: React.FC = () => (
   <Page title="Contact">
     <Row>
       <Col md={8}>
-        <Heading className="mb-md-4">Contact</Heading>
+        <H1>Contact</H1>
       </Col>
       <Col md={4} className="text-md-right">
-        {/* TODO: style */}
-        <OutlineButton
-          className="my-2 mt-md-1"
-          href="mailto:kornbluthart@gmail.com"
-        >
-          Contact Us
-        </OutlineButton>
+        <ContactButton>Contact Us</ContactButton>
       </Col>
       <Col md={10}>
-        <HorizontalRule className="mb-4" />
-        <Heading as="h2">Acquisitions</Heading>
+        <Divider />
+        <H2>Acquisitions</H2>
         <Paragraph>
           Available art can be viewed by clicking here (TODO). Additional
           artwork may be available; please contact us if you are looking for
           something specific. We are happy to assist collectors and institutions
           in selecting pieces of a particular medium, size, period or series.
         </Paragraph>
-        <HorizontalRule className="mb-4" />
-        <Heading as="h2">Additions</Heading>
+        <Divider />
+        <H2>Additions</H2>
         <Paragraph>
           To have your Frances Kornbluth artwork added to the{' '}
           <Link to={Url.CollectionPage}>collection page</Link>, please send us a
@@ -48,24 +43,24 @@ export const Contact: React.FC = () => (
           <Link to={Url.WantedPage}>Click here</Link> to see some of the pieces
           for which we are currently seeking images.
         </Paragraph>
-        <HorizontalRule className="mb-4" />
-        <Heading as="h2">Consignments</Heading>
+        <Divider />
+        <H2>Consignments</H2>
         <Paragraph>
           To have your Frances Kornbluth artwork added to the available art page
           (TODO), please follow the instructions under additions, also
           indicating that you would like to sell the piece. We will contact you
           to discuss consignment terms.
         </Paragraph>
-        <HorizontalRule className="mb-4" />
-        <Heading as="h2">Expositions</Heading>
+        <Divider />
+        <H2>Expositions</H2>
         <Paragraph>
           Some of the artwork on the{' '}
           <Link to={Url.CollectionPage}>collection page</Link> can be made
           available for public exhibition; please contact us to discuss
           availability and lending arrangements.
         </Paragraph>
-        <HorizontalRule className="mb-4" />
-        <Heading as="h2">Reproductions</Heading>
+        <Divider />
+        <H2>Reproductions</H2>
         <Paragraph>
           If you would like to reproduce any of Frances Kornbluth’s artwork for
           any purpose, please contact us. See the{' '}
@@ -76,3 +71,15 @@ export const Contact: React.FC = () => (
     </Row>
   </Page>
 );
+
+const H1 = styled(Heading).attrs({ className: 'mb-md-3' })``;
+
+const H2 = styled(Heading).attrs({ as: 'h2', color: 'gray' })``;
+
+const Divider = styled(HorizontalRule).attrs({ className: 'mb-4' })``;
+
+// TODO: style
+const ContactButton = styled(OutlineButton).attrs({
+  className: 'my-2 mt-md-1',
+  href: 'mailto:kornbluthart@gmail.com',
+})``;
--- src/components/styled/Heading.ts
+++ src/components/styled/Heading.ts
@@ -1,8 +1,12 @@
 import styled from 'styled-components';
 import { colors, typography, getRems } from '../../styles';
 
-export const Heading = styled.h1`
-  color: ${colors.darkGray};
+interface IHeadingProps {
+  color?: 'darkGray' | 'gray';
+}
+
+export const Heading = styled.h1<IHeadingProps>`
+  color: ${({ color }) => (color ? colors[color] : colors.darkGray)};
   font-family: ${typography.heading};
   font-weight: 500;
   letter-spacing: ${getRems(2)};
