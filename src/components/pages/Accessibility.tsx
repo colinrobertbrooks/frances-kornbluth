c8ccdef5915bcd311193d2921beb822562b9d0cd
Colin Brooks <colinrobertbrooks@gmail.com>
2020-12-05 10:07:00 -0500

    a11y page

--- src/components/pages/Accessibility.tsx
+++ src/components/pages/Accessibility.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Page } from '../layout';
+import { Link, Url, ExternalLink } from '../router';
 import { Row, Col, Heading, Paragraph } from '../styled';
 
 export const Accessibility: React.FC = () => (
@@ -7,7 +8,16 @@ export const Accessibility: React.FC = () => (
     <Row>
       <Col md={12}>
         <Heading className="mb-3">Accessibility</Heading>
-        <Paragraph>TODO</Paragraph>
+        <Paragraph>
+          We are committed to making the information on our website accessible
+          to all visitors, including people with disabilities. If you encounter
+          content that you are unable to access due to a{' '}
+          <ExternalLink href="https://www.w3.org/WAI/fundamentals/accessibility-intro/">
+            web accessibility issue
+          </ExternalLink>
+          , or have any suggestions on accessibility improvements, please{' '}
+          <Link to={Url.ContactPage}>contact us</Link>.
+        </Paragraph>
       </Col>
     </Row>
   </Page>
