f87cea20fff99ac5c00abcfe010697be764fadb4
Colin Brooks <colinrobertbrooks@gmail.com>
2020-12-08 08:23:48 -0500

    refactor hooks

--- src/hooks/useDocumentTitle.ts
+++ src/hooks/useDocumentTitle.ts
@@ -1,7 +1,7 @@
 import { useEffect } from 'react';
 
-export const useDocumentTitle = (title: string): void => {
+export function useDocumentTitle(title: string): void {
   useEffect(() => {
     document.title = title;
   }, [title]);
-};
+}
