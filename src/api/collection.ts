3dc2d6488e9bfa274b022c02107604ed05e0d4ef
Colin Brooks <colinrobertbrooks@gmail.com>
2020-12-08 23:47:24 -0500

    add type

--- src/api/collection.ts
+++ src/api/collection.ts
@@ -12,7 +12,7 @@ const deserializeCollection = (
       minImgSrc: gsx$minimgsrc.$t,
     }));
 
-export const getCollection = async (): Promise<any> => {
+export const getCollection = async (): Promise<ICollectionRecord[]> => {
   try {
     const response = await adapter.get(
       '/list/1IzGaO3pLokvuiuKkS2UccK4KuYvnQwM-osJf3WfciJU/1/public/full?alt=json'
