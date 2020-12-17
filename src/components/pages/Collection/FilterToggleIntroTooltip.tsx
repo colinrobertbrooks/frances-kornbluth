ec841d39ce1a1cbc770ba0bfdac1fc94a22edd6d
Colin Brooks <colinrobertbrooks@gmail.com>
2020-12-16 19:41:52 -0500

    set popover typography

--- src/components/pages/Collection/FilterToggleIntroTooltip.tsx
+++ src/components/pages/Collection/FilterToggleIntroTooltip.tsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import styled from 'styled-components';
 import { Popover as ReactstrapPopper, PopoverBody } from 'reactstrap';
-import { media } from '../../../styles';
+import { media, typography } from '../../../styles';
 import { useLocalStorage, useTimeout } from '../../../hooks';
 
 const FilterToggleIntroTooltip: React.FC = () => {
@@ -36,6 +36,7 @@ const FilterToggleIntroTooltip: React.FC = () => {
 
 const Popover = styled(ReactstrapPopper)`
   .popover {
+    font-family: ${typography.default};
     max-width: 250px;
     z-index: 999;
 
