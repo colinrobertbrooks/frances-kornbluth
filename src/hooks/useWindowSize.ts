39e4c355892d76a70544d636916f63e30e419860
Colin Brooks <colinrobertbrooks@gmail.com>
2021-06-08 09:53:25 -0400

    debounce useWindowSize

--- src/hooks/useWindowSize.ts
+++ src/hooks/useWindowSize.ts
@@ -1,26 +1,21 @@
 import { useState, useEffect } from 'react';
+import { debounce } from '../utils';
 
 export interface IWindowSize {
   width: number;
   height: number;
 }
 
+const getWindowSize = () => ({
+  width: window.innerWidth,
+  height: window.innerHeight,
+});
+
 export function useWindowSize(): IWindowSize {
-  const [windowSize, setWindowSize] = useState<{
-    width: number;
-    height: number;
-  }>({
-    width: window.innerWidth,
-    height: window.innerHeight,
-  });
+  const [windowSize, setWindowSize] = useState<IWindowSize>(getWindowSize);
 
   useEffect(() => {
-    const onResize = () =>
-      setWindowSize({
-        width: window.innerWidth,
-        height: window.innerHeight,
-      });
-
+    const onResize = debounce(() => setWindowSize(getWindowSize), 250);
     window.addEventListener('resize', onResize);
     return () => window.removeEventListener('resize', onResize);
   }, []);
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -2,3 +2,4 @@ export * from './arrays';
 export * from './collection';
 export * from './env';
 export * from './gtm';
+export * from './perf';
