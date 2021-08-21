75935d4e86beab4bfc496ec8cd8d8a7b3a67d62d
Colin Brooks <colinrobertbrooks@gmail.com>
2021-08-20 23:20:52 -0400

    update collection deserialization

--- src/api/collection.ts
+++ src/api/collection.ts
@@ -1,52 +1,50 @@
 import { adapter } from './utils';
-import {
-  Decade,
-  ISerializedCollectionRecord,
-  ICollectionRecord,
-} from '../types';
+import { Decade, Status, ICollectionRecord, TItem } from '../types';
 import { deriveMediumGroup, deriveSizeGroup, generateTags } from '../utils';
 
-const deserializeCollection = (
-  entry: ISerializedCollectionRecord[]
-): ICollectionRecord[] =>
-  entry
-    .filter(({ gsx$showonwebsite }) => gsx$showonwebsite.$t)
-    .map(
-      ({
-        gsx$id,
-        gsx$title,
-        gsx$minimgsrc,
-        gsx$year,
-        gsx$decade,
-        gsx$medium,
-        gsx$dimensions,
-        gsx$status,
-        gsx$holder,
-        gsx$newonwebsite,
-        ...gsx$rest
-      }) => {
-        const year = gsx$year.$t;
-        const decade = gsx$decade.$t;
-        const medium = gsx$medium.$t;
-        const dimensions = gsx$dimensions.$t;
+const deserializeCollection = (values: string[][]): ICollectionRecord[] => {
+  const [headRow, ...bodyRows] = values;
+  const collection: TItem[] = [];
+  bodyRows.forEach((row) => {
+    const item: TItem = {};
+    row.forEach((val, valIdx) => {
+      item[headRow[valIdx]] = val;
+    });
+    collection.push(item);
+  });
 
-        return {
-          id: Number(gsx$id.$t),
-          title: gsx$title.$t || 'Untitled',
-          minImgSrc: gsx$minimgsrc.$t,
-          year: year ? Number(year) : null,
-          decade: decade ? (decade as Decade) : Decade.Unknown,
-          medium,
-          mediumGroup: deriveMediumGroup(medium),
-          dimensions: dimensions || 'finished size unavailable',
-          sizeGroup: deriveSizeGroup(dimensions),
-          status: gsx$status.$t,
-          holder: gsx$holder.$t || null,
-          tags: generateTags(gsx$rest),
-          isNew: !!gsx$newonwebsite.$t,
-        };
-      }
-    );
+  return collection.map(
+    ({
+      id,
+      title,
+      minImgSrc,
+      year,
+      decade,
+      medium,
+      dimensions,
+      status,
+      holder,
+      newOnWebsite,
+      ...rest
+    }) => {
+      return {
+        id: Number(id),
+        title: title || 'Untitled',
+        minImgSrc,
+        year: year ? Number(year) : null,
+        decade: decade ? (decade as Decade) : Decade.Unknown,
+        medium,
+        mediumGroup: deriveMediumGroup(medium),
+        dimensions: dimensions || 'finished size unavailable',
+        sizeGroup: deriveSizeGroup(dimensions),
+        status: status as Status,
+        holder: holder || null,
+        tags: generateTags(rest),
+        isNew: !!newOnWebsite,
+      };
+    }
+  );
+};
 
 export const getCollection = async (): Promise<ICollectionRecord[]> => {
   const worksheetId = '1IzGaO3pLokvuiuKkS2UccK4KuYvnQwM-osJf3WfciJU';
@@ -57,7 +55,7 @@ export const getCollection = async (): Promise<ICollectionRecord[]> => {
     const response = await adapter.get(
       `${worksheetId}/values/${tabName}?alt=json&key=${keyValue}`
     );
-    return deserializeCollection(response.data.feed.entry);
+    return deserializeCollection(response.data.values);
   } catch (error) {
     throw new Error('Error fetching collection');
   }
