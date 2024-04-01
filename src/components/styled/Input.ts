1aecd564d106f7418fcc3d1d9115aebf62568773
Colin Brooks <colinrobertbrooks@gmail.com>
2024-04-01 16:01:08 -0400

    refactor: mixins

--- src/components/styled/Input.ts
+++ src/components/styled/Input.ts
@@ -1,12 +1,16 @@
 import { Input as ReactstrapInput, InputProps } from 'reactstrap';
 import styled from 'styled-components';
-import { colors, typography } from '../../styles';
+import { colors, disabledCSS, focusBorderCSS, typography } from '../../styles';
 
 export const Input = styled(ReactstrapInput)<InputProps>`
+  border-color: ${colors.lightGray};
   font-family: ${typography.default};
 
   &:focus {
-    border-color: #ced4da;
-    box-shadow: 0 0 0 0.2rem ${colors.lightRed};
+    ${focusBorderCSS}
+  }
+
+  &:disabled {
+    ${disabledCSS}
   }
 `;
