cbaee4933b4495b252dcf7710877dfd5ed08c34b
Colin Brooks <colinrobertbrooks@gmail.com>
2024-04-01 08:30:50 -0400

    fix: file name

--- src/assets/pages/publications/index.ts
+++ src/assets/pages/publications/index.ts
@@ -1,6 +1,6 @@
 import monhegan1995Img from './monhegan-1995.jpg';
 import explorations2011Img from './explorations-2011.jpg';
-import maineWomenPioneers2013Img from './maine-women-pioneers-2012.jpg';
+import maineWomenPioneers2013Img from './maine-women-pioneers-2013.jpg';
 import onIsland2013Img from './on-island-2013.jpg';
 
 export {
