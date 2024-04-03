3431ef20135229a2177bb1371599850c9cd3787d
Colin Brooks <colinrobertbrooks@gmail.com>
2024-04-03 11:56:55 -0400

    refactor: --indentation

--- src/components/layout/Banner.tsx
+++ src/components/layout/Banner.tsx
@@ -2,21 +2,19 @@ import styled from 'styled-components';
 import { UnstyledLink, Url } from '../../router';
 import { BANNER_HEIGHT_PX, colors } from '../../styles';
 
-const Banner = () => {
-  return (
-    <Element>
-      <UnstyledLink className="text-center" to={Url.ExhibitionsPage}>
-        <span>
-          See <em>Women Artists of Monhegan Island: A Common Bond</em> at the
-          Mongegan Museum
-        </span>
-        <span className="small d-none d-md-block">
-          July 1 – September 30, 2024
-        </span>
-      </UnstyledLink>
-    </Element>
-  );
-};
+const Banner = () => (
+  <Element>
+    <UnstyledLink className="text-center" to={Url.ExhibitionsPage}>
+      <span>
+        See <em>Women Artists of Monhegan Island: A Common Bond</em> at the
+        Mongegan Museum
+      </span>
+      <span className="small d-none d-md-block">
+        July 1 – September 30, 2024
+      </span>
+    </UnstyledLink>
+  </Element>
+);
 
 const Element = styled.aside`
   align-items: center;
