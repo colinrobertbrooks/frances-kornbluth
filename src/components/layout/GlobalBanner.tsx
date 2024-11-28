1d377351e85d4e5ae2edbd5f054e4d4230fe76ec
Colin Brooks <colinrobertbrooks@gmail.com>
2024-11-28 12:40:30 -0500

    content: stage wildlands@bowdoin banner

--- src/components/layout/GlobalBanner.tsx
+++ src/components/layout/GlobalBanner.tsx
@@ -1,7 +1,19 @@
 import styled from 'styled-components';
+import { UnstyledLink, Url } from 'router';
 import { GLOBAL_BANNER_HEIGHT_PX, colors } from '../../styles';
 
-const GlobalBanner = () => <Element>{/* ... */}</Element>;
+const GlobalBanner = () => (
+  <Element>
+    <UnstyledLink className="text-center" to={Url.ExhibitionsPage}>
+      <span>
+        See <em>The Monhegan Wildlands</em> at the Bowdoin College Museum of Art
+      </span>
+      <span className="small d-none d-md-block">
+        December 12, 2024 - June 1, 2025
+      </span>
+    </UnstyledLink>
+  </Element>
+);
 
 const Element = styled.aside`
   align-items: center;
