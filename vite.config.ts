06c206d8178f18932f634af98c53000ae159cdd9
Colin Brooks <colinrobertbrooks@gmail.com>
2025-03-20 13:47:05 -0400

    fix: gh-pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import tsconfigPathsPlugin from 'vite-tsconfig-paths';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/',
+  base: '/frances-kornbluth/',
   plugins: [reactPlugin(), tsconfigPathsPlugin()],
   server: {
     port: 3000,
